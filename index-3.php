e4d5342244e673e84e21671a0a4a5f5ac3d32e03
Keyllian Azevedo <157046442+Keyllian7@users.noreply.github.com>
2024-10-24 17:01:01 -0300

    Exerc 3

---
+++ index-3.php
@@ -0,0 +1,46 @@
+Desafio 2: Sistema de Funcionários
+Crie uma classe Funcionario com os seguintes atributos:
+- nome
+- salario
+- cargo
+
+Métodos:
+- aumentar_salario(percentual): Aumenta o salário em uma porcentagem.
+- __str__(): Retorna as informações do funcionário.
+
+
+<?php
+
+class Funcionario {
+    private $nome;
+    private $salario;
+    private $cargo;
+
+    public function __construct($n, $s, $c) {
+        $this->nome = $n;
+        $this->salario = $s;
+        $this->cargo = $c;
+    }
+    public function aumentar_salario($porcentagem): string {
+        $aumento = $this->salario * ($porcentagem / 100);
+        $this->salario += $aumento;
+        return "Esse é o seu novo salário com aumento: R$". $this->salario;
+    }
+
+    public function __tostring(): string{
+        return "Nome: $this->nome | Salário: $this->salario | Cargo: $this->cargo";
+    }
+
+    public function getSalario(){
+        return $this->salario;
+    }
+    
+}
+
+echo $avila = new Funcionario("Avila", 1000, "Gerente"); 
+echo "<br>";
+echo $avila->aumentar_salario(30);
+echo "<br>";
+echo $avila->getSalario()
+
+?>
